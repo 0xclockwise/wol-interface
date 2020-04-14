ebc6e95a9697983097cd52e19a65b9d686f352c9
ClockWise <johnstonethepepsi@gmail.com>
2020-04-14 10:16:39 +0200

    add code for adding users

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ app.use('/static', express.static('static'))
 app.set("view engine", "pug")
 app.set("views", path.join(__dirname, "views"))
 app.use(cors())
-app.use(bodyParser.json());
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/', (req, res) => {
     console.log(`Sending WoL signal to ${req.body.mac}`)
@@ -30,6 +31,18 @@ app.all('/', function (req, res) {
         computers: computers
     })
     console.log('Request incoming')
+})
+
+app.get('/add', (req, res) => {
+    res.render('add')
+})
+
+app.post('/add', (req, res) => {
+    console.log(req.body)
+    let buffer = JSON.stringify(readEntries().concat(req.body), null, 4)
+    fs.writeFileSync('computers.json', buffer)
+    res.redirect('/add')
+    
 })
 
 
